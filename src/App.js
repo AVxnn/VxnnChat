a990388abedad51407e1211b3713f62abe2587dc
Vxnn <george@thevann.ru>
2022-06-09 00:34:08 +0500

    2022-09-06 v1.73 Fix bags 1.72

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     }
 
     useEffect(() => {
-        window.addEventListener('unload', leaveHandler);
+        window.addEventListener('beforeunload', leaveHandler);
     }, [])
 
     return (
